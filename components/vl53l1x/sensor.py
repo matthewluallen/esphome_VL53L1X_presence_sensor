0a7b5a939425f312af922f3b03c67ae96ea12f2a
Matthew Luallen <mluallen@illinois.edu>
2023-11-21 14:26:36 -0600

    Update sensor.py

--- components/vl53l1x/sensor.py
+++ components/vl53l1x/sensor.py
@@ -16,13 +16,13 @@ vl53l1x_distance_modes = {
     'LONG': vl53l1x_distance_mode.LONG,
 }
 
-CONF_DISTANCE_MODE = 'distance_mode'
+CONF_DISTANCE_MODE = "distance_mode"
 CONF_TIMING_BUDGET = 'timing_budget'
 CONF_RETRY_BUDGET = 'retry_budget'
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_METER, ICON_ARROW_EXPAND_VERTICAL, 2).extend({
     cv.GenerateID(): cv.declare_id(VL53L1XSensor),
-    cv.Optional(CONF_DISTANCE_MODE, default="LONG"): cv.enum(vl53l1x_distance_modes),
+    cv.Optional(CONF_DISTANCE_MODE, default="LONG"): cv.enum(vl53l1x_distance_modes, upper=True),
     cv.Optional(CONF_TIMING_BUDGET, default='50ms'):
         cv.All(cv.positive_time_period_microseconds,
                cv.Range(min=TimePeriod(microseconds=20000),
@@ -30,21 +30,12 @@ CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.Optional(CONF_RETRY_BUDGET, default=5): cv.int_range(min=0, max=255),
 }).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x29))
 
-SETTERS = {
-
-  CONF_DISTANCE_MODE: 'set_distance_mode',
-  CONF_TIMING_BUDGET: 'set_timing_budget',
-  CONF_RETRY_BUDGET: 'set_retry_budget',
-
-}
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
-
-    for key, setter in SETTERS.items():
-        if key in config:
-            cg.add(getattr(var, setter)(config[key]))
-
+    cg.add(var.set_distance_mode(config[CONF_DISTANCE_MODE]))
+    cg.add(var.set_timing_budget(config[CONF_TIMING_BUDGET]))
+    cg.add(var.set_retry_budget(config[CONF_RETRY_BUDGET]))
     yield sensor.register_sensor(var, config)
     yield i2c.register_i2c_device(var, config)
